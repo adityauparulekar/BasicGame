97503642912550158e268ff3a96ecfc92cc014bc
Aditya Parulekar <adityaparulekar@Adityas-MBP.attlocal.net>
2018-07-31 15:26:45 -0500

    10b10

--- basic_game/nnetwork.py
+++ basic_game/nnetwork.py
@@ -4,15 +4,15 @@ class NeuralNetwork():
 
     def __init__(self, mutation_rate, mom=None, dad=None):
         if not mom is None:
-            cross_over1 = np.random.choice(2, (30,41), p=[0.5, 0.5])
-            self.weights1 = mutation_rate*np.random.randn(30, 41)+np.multiply(cross_over1,dad)+np.multiply(1-cross_over1, mom)
-            cross_over2 = np.random.choice(2, (10,31), p=[0.5, 0.5])
-            self.weights2 = mutation_rate*np.random.randn(10, 31)+np.multiply(cross_over2, dad)+np.multiply(1-cross_over2, mom)
+            cross_over1 = np.random.choice(2, (10,41), p=[0.5, 0.5])
+            self.weights1 = mutation_rate*np.random.randn(10, 41)+np.multiply(cross_over1,dad)+np.multiply(1-cross_over1, mom)
+            cross_over2 = np.random.choice(2, (10,11), p=[0.5, 0.5])
+            self.weights2 = mutation_rate*np.random.randn(10, 11)+np.multiply(cross_over2, dad)+np.multiply(1-cross_over2, mom)
             cross_over3 = np.random.choice(2, (3,11), p=[0.5, 0.5])
             self.weights3 = mutation_rate*np.random.randn(3, 11)+np.multiply(cross_over3, dad)+np.multiply(1-cross_over3, mom)
         else:
-            self.weights1 = mutation_rate*np.random.randn(30, 41)
-            self.weights2 = mutation_rate*np.random.randn(10, 31)
+            self.weights1 = mutation_rate*np.random.randn(10, 41)
+            self.weights2 = mutation_rate*np.random.randn(10, 11)
             self.weights3 = mutation_rate*np.random.randn(3, 11)
 
     def sigmoid(self, x):
