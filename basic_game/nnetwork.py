3c21cae3c0e5174cebd2dc1e92b39e78b23efb8f
AdvaitParulekar <adutheparulekar@tamu.edu>
2018-07-31 13:09:29 -0500

    laath

--- basic_game/nnetwork.py
+++ basic_game/nnetwork.py
@@ -2,10 +2,18 @@ import math
 import numpy as np
 class NeuralNetwork():
 
-    def __init__(self):
-        self.weights1 = np.random.randn(40, 30)
-        self.weights2 = np.random.randn(30, 10)
-        self.weights3 = np.random.randn(10, 3)
+    def __init__(self, mutation_rate, mom=None, dad=None):
+        if not mom is None:
+            cross_over1 = n.random.choice(2, (41,30), p=[0.5, 0.5])
+            self.weights1 = mutation_rate*np.random.randn(41, 30)+np.multiply(cross_over1, dad)+np.multiply(1-cross_over1, mom)
+            cross_over2 = n.random.choice(2, (31,10), p=[0.5, 0.5])
+            self.weights2 = mutation_rate*np.random.randn(31, 10)+np.multiply(cross_over2, dad)+np.multiply(1-cross_over2, mom)
+            cross_over3 = n.random.choice(2, (11,3), p=[0.5, 0.5])
+            self.weights3 = mutation_rate*np.random.randn(11, 3)+np.multiply(cross_over3, dad)+np.multiply(1-cross_over3, mom)
+        else:
+            self.weights1 = mutation_rate*np.random.randn(41, 30)
+            self.weights2 = mutation_rate*np.random.randn(31, 10)
+            self.weights3 = mutation_rate*np.random.randn(11, 3)
 
     def sigmoid(x):
         return 1/(math.exp(-x)+1)
--- basic_game/player.py
+++ basic_game/player.py
@@ -5,13 +5,17 @@ from nnetwork import *
 PLAYER_SCALING = 60/3016
 
 class Player():
-    def __init__(self):
+    def __init__(self, mom = None, dad = None):
         self.player_sprite = arcade.Sprite("character.png", PLAYER_SCALING)
         self.player_sprite.center_x = 64
         self.player_sprite.center_y = 250
         self.player_move = 0
-
-        self.brain = NeuralNetwork()
+        self.mom = mom
+        self.dad = dad
+        if not mom is None:
+            self.brain = NeuralNetwork(1)
+        else:
+            self.brain = NeuralNetwork(mom, dad, 0.1)
 
     def draw(self):
         self.player_sprite.draw()
